b1106caed621640836698ba684ea837e57a1a3af
koo <kooym5@naver.com>
2020-08-05 19:10:15 +0900

    gogo

---
+++ 2020-08-05-기본개념.py
@@ -0,0 +1,53 @@
+a="Python"
+print(a)
+
+a=3
+if a>1 :
+    print("a is greater than 1")
+
+for a in (1,2,3) :
+    print(a)
+
+i=0
+while i<3:
+    i=i+1
+    print(i)
+
+def add(a,b):
+    return a+b
+a= add(3,4)
+print(a)
+
+a=3**4
+print(a)
+
+print(3%7)
+print(7%3)
+print(7/4)
+print(7//4)
+say="\"python\'s favorite food is perl\" he said "
+print(say)
+multiline="Life is too short\nYou need python"
+print(multiline)
+multiline2="""Life is too short
+You need python"""
+print(multiline2)
+
+a="Life is too short"
+print(len(a))
+print(a[12])
+b=a[0:3]
+print(b)
+c=a[:]
+print(c)
+
+a="%10.4f" % 3.42134324
+print(a)
+a="hobby"
+print(a.count('b'))
+print(a.find('o'))
+print(bool('python'))
+print(bool(''))
+
+number = input("숫자를 입력하세요")
+print (number)
